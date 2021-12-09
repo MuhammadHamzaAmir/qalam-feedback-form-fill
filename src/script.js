a904280fba31ef873bdd116370057783497de259
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2021-12-09 14:55:04 +0500

    !!!PIKA PIAK!!! Piakchu meme added

--- src/script.js
+++ src/script.js
@@ -92,3 +92,25 @@ let login = async (id,pass,page) =>{
 // ii;;;;;;::;;;;;;;;iiiiiii;iiiiiiiiiiiiii;;;;;;:::,,:::,,,..,,..    .,,.........
 // 
 
+//                 .
+//                .;;:,.
+//                 ;iiii;:,.                                   .,:;.
+//                 :i;iiiiii:,                            .,:;;iiii.
+//                  ;iiiiiiiii;:.                    .,:;;iiiiii;i:
+//                   :iiiiiiiiiii:......,,,,,.....,:;iiiiiiiiiiii;
+//                    ,iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii:
+//                     .:iii;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;,
+//                       .:;;iiiiiiiiiiiiiiiiiiiiiiiiiii;;ii;,
+//                        :iiii;;iiiiiiiiiiiiiii;;iiiiiii;:.
+//                       ,iiii;1f:;iiiiiiiiiiii;if;:iiiiiii.
+//                      .iiiii:iL..iiiiiiiiiiii;:f: iiiiiiii.
+//                      ;iiiiii:.,;iiii;iiiiiiii:..:iiiiiiii:
+//                     .i;;;iiiiiiiiii;,,;iiiiiiiiiiii;;iiiii.
+//                     ::,,,,:iiiiiiiiiiiiiiiiiiiiii:,,,,:;ii:
+//                     ;,,,,,:iiiiiiii;;;;;;;iiiiii;,,,,,,;iii.
+//                     ;i;;;;iiiiiiii;:;;;;;:iiiiiii;::::;iiii:
+//                     ,iiiiiiiiiiiiii;;;;;;:iiiiiiiiiiiiiiiiii.
+//                      .iiiiiiiiiiiiii;;;;;iiiiiiiiiiiiiiiiiii:
+//                       .;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii;
+//                        ;iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.
+//                       .;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
